601718ba77729e064bf98296a004604468b13a9a
NishanthMolleti <48158364+NishanthMolleti@users.noreply.github.com>
2020-03-09 12:58:51 +0530

    Update arraygrid.py

--- arraygrid.py
+++ arraygrid.py
@@ -6,6 +6,11 @@ RED = (255,0,0)
 W=10
 H=10
 M=2
+a=2
+r1=0
+c1=0
+c2=0
+r2=0
 grid=[]
 for row in range (50):
     grid.append([])
@@ -13,7 +18,7 @@ for row in range (50):
         grid[row].append(0)
 
 pygame.init()
-WINSIZE=[600,600]
+WINSIZE=[602,602]
 sc=pygame.display.set_mode(WINSIZE)
 pygame.display.set_caption("PATH FINDER")
 done=False
@@ -27,7 +32,14 @@ while not done:
              column=pos[0]//(W+M)
              row=pos[1]//(H+M)
              grid[row][column]=1
-             print ("Click",pos,"Grid Coordinates: ",row ,column)
+             a=a-1
+             if(a==1):
+                 r1=row
+                 c1=column
+             if(a==0):
+                 r2=row
+                 c2=column
+             print ("Click",pos,"Grid Coordinates: ",row ,column,"r1,c1=",r1 ,c1,"r2,c2=", r2 ,c2)
      sc.fill(BLACK)
      for row in range(50):
         for column in range(50):
@@ -35,6 +47,7 @@ while not done:
             if grid[row][column]==1:
                 color=GREEN
             pygame.draw.rect(sc,color,[(M+W)*column+M,(M+H)*row+M,W,H])
+            
      clock.tick(60)
      pygame.display.flip()
 pygame.quit()
